1f917426afa103871c1a18e23f73c8814232dd55
matrakei <48519913@est.ort.edu.ar>
2024-06-03 09:24:16 -0300

    Arrays Vehiculo

---
+++ Assets/VEHICULOSMANAGER.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VEHICULOSMANAGER : MonoBehaviour
+{
+
+    public Vehiculo vehiculoScript;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int cantRuedasVehiculo = vehiculoScript.cantRuedas;
+        Debug.Log("Cant Ruedas: " + cantRuedasVehiculo);
+        if (cantRuedasVehiculo < 6) 
+        {
+            vehiculoScript.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
